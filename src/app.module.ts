45b997d9fb729b2e15aef8d57e9ba165e253b86d
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-09-17 13:27:25 +0545

    graphql added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URL!),
+    BookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
