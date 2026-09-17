c721695f2dab88e6ab69b4f2f3960e0d6ed35f43
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-09-17 14:06:36 +0545

    added dtos sand model

---
+++ src/book/model/book.model.ts
@@ -0,0 +1,24 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+@ObjectType()
+export class Book extends Document {
+  @Field(() => ID)
+  declare readonly _id: string;
+
+  @Prop({ required: true })
+  @Field()
+  title: string;
+
+  @Prop()
+  @Field({ nullable: true })
+  description?: string;
+
+  @Prop({ required: true })
+  @Field()
+  author: string;
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
